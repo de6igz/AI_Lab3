cac7f53916c63e560ed5f3d297d8bde2f81cd369
de6igz <Solopov2003@gmail.com>
2023-10-02 20:07:10 +0300

    [~] cleaned comments

--- cmd/main.go
+++ cmd/main.go
@@ -18,23 +18,5 @@ func main() {
 		base,
 		userInput,
 	)
-	//// Отправка запросов к базе знаний
-	//query := "character(X)."
-	//result, err := kb.Query(query)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//// Вывод результатов запроса
-	//fmt.Println("Друзья Наруто:")
-	//for result.Next() {
-	//	var s struct {
-	//		Friend string `prolog:"X"`
-	//	}
-	//	//fmt.Printf("%v", result)
-	//	if err := result.Scan(&s); err != nil {
-	//		panic(err)
-	//	}
-	////	fmt.Printf("Who = %s\n", s.Friend) // ==> Who = socrates
-	//}
+
 }
